81047dd964a5d1221ca515a96f2c12a83f295458
healym3 <79485702+healym3@users.noreply.github.com>
2022-02-07 11:57:50 -0600

    Added Data Binding Object

--- app/src/main/java/com/example/bio/MainActivity.java
+++ app/src/main/java/com/example/bio/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.bio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -9,21 +10,33 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.bio.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     private EditText enterHobbies;
     private TextView hobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        enterHobbies = findViewById(R.id.enterHobbies);
-        hobbies = findViewById(R.id.hobbiesText);
+//        setContentView(R.layout.activity_main);
+//        enterHobbies = findViewById(R.id.enterHobbies);
+//        hobbies = findViewById(R.id.hobbiesText);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addHobbies(view);
+            }
+        });
     }
 
     public void addHobbies(View view) {
-        hobbies.setText("Hobbies: " + enterHobbies.getText().toString().trim());
-        hobbies.setVisibility(View.VISIBLE);
+//        hobbies.setText("Hobbies: " + enterHobbies.getText().toString().trim());
+//        hobbies.setVisibility(View.VISIBLE);
+        binding.hobbiesText.setText("Hobbies: " + binding.enterHobbies.getText().toString().trim());
+        binding.hobbiesText.setVisibility(View.VISIBLE);
 
         //hide keyboard
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
